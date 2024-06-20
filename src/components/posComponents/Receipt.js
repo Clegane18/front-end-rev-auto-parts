fdb68e870d5b516dcdd51ae7a5b477eefb0d7b2a
Clegane18 <invictuslanzeta@gmail.com>
2024-06-21 01:31:40 +0800

    added a back button in the receipt component

--- src/components/posComponents/Receipt.js
+++ src/components/posComponents/Receipt.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 import "../../styles/Receipt.css";
 
 const Receipt = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { receipt } = location.state || {};
 
   if (!receipt) {
@@ -20,37 +22,40 @@ const Receipt = () => {
   };
 
   return (
-    <div className="receipt-container">
-      <div className="store-info">
-        <h2>G&F Auto Supply</h2>
-        <p>1104 Gov Fortunato Halili Rd, Santa Maria, Bulacan, Philippines</p>
+    <div className="page-container">
+      <FaArrowLeft className="back-icon" onClick={() => navigate(-2)} />
+      <div className="receipt-container">
+        <div className="store-info">
+          <h2>G&F Auto Supply</h2>
+          <p>1104 Gov Fortunato Halili Rd, Santa Maria, Bulacan, Philippines</p>
+        </div>
+        <div className="receipt-header">
+          <h3>Receipt</h3>
+          <p>Transaction No: {receipt.transactionNo}</p>
+          <p>Date: {new Date(receipt.transactionDate).toLocaleString()}</p>
+        </div>
+        <ul className="receipt-items">
+          {receipt.items.map((item, index) => (
+            <li key={index} className="item-details">
+              <div className="quantity-container">
+                <span>Qty:</span>
+                <span>{item.quantity}</span>
+              </div>
+              <span className="item-name">{item.productName}</span>
+              <span>Amount: ₱{formatCurrency(item.unitPrice)}</span>
+              <span>Subtotal: ₱{formatCurrency(item.subtotalAmount)}</span>
+            </li>
+          ))}
+        </ul>
+        <div className="totals">
+          <p>Total Amount: ₱{formatCurrency(receipt.totalAmount)}</p>
+          <p>Payment Amount: ₱{formatCurrency(receipt.paymentAmount)}</p>
+          <p>Change: ₱{formatCurrency(receipt.change)}</p>
+        </div>
+        <button className="print-button" onClick={handlePrint}>
+          Print Receipt
+        </button>
       </div>
-      <div className="receipt-header">
-        <h3>Receipt</h3>
-        <p>Transaction No: {receipt.transactionNo}</p>
-        <p>Date: {new Date(receipt.transactionDate).toLocaleString()}</p>
-      </div>
-      <ul className="receipt-items">
-        {receipt.items.map((item, index) => (
-          <li key={index} className="item-details">
-            <div className="quantity-container">
-              <span>Qty:</span>
-              <span>{item.quantity}</span>
-            </div>
-            <span className="item-name">{item.productName}</span>
-            <span>Amount: ₱{formatCurrency(item.unitPrice)}</span>
-            <span>Subtotal: ₱{formatCurrency(item.subtotalAmount)}</span>
-          </li>
-        ))}
-      </ul>
-      <div className="totals">
-        <p>Total Amount: ₱{formatCurrency(receipt.totalAmount)}</p>
-        <p>Payment Amount: ₱{formatCurrency(receipt.paymentAmount)}</p>
-        <p>Change: ₱{formatCurrency(receipt.change)}</p>
-      </div>
-      <button className="print-button" onClick={handlePrint}>
-        Print Receipt
-      </button>
     </div>
   );
 };
