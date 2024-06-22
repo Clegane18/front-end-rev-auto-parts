a13396e8052efb65956abb9fdc316020235c9eb1
Clegane18 <invictuslanzeta@gmail.com>
2024-06-22 19:55:52 +0800

    integrate edit information functionality

--- src/services/inventory-api.js
+++ src/services/inventory-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3002/api/auth", // Change this to your server URL
+  baseURL: "http://localhost:3002/api/auth",
 });
 
 // Product APIs
