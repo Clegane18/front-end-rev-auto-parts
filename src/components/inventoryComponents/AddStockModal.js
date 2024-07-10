5c8678455d094779728a2d61d930c946fe769c1e
Clegane18 <invictuslanzeta@gmail.com>
2024-07-10 15:04:44 +0800

    adjus the design of add stock modal

--- src/components/inventoryComponents/AddStockModal.js
+++ src/components/inventoryComponents/AddStockModal.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { addToProductStock } from "../../services/inventory-api"; // Adjust the import path as necessary
-import "../../styles/inventoryComponents/AddStockModal.css"; // Make sure you have styles defined
+import { addToProductStock } from "../../services/inventory-api";
+import "../../styles/inventoryComponents/AddStockModal.css";
 
 const AddStockModal = ({ product, onClose, onSave }) => {
   const [quantity, setQuantity] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSave = async () => {
+  const handleSave = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
     try {
       if (quantity <= 0 || isNaN(quantity)) {
         setErrorMessage("Please enter a valid quantity.");
@@ -14,8 +15,8 @@ const AddStockModal = ({ product, onClose, onSave }) => {
       }
 
       const response = await addToProductStock(product.id, quantity);
-      onSave(response.product); // Pass the updated product back to the parent component
-      onClose(); // Close the modal
+      onSave(response.product);
+      onClose();
     } catch (error) {
       console.error("Error adding product stock:", error);
       setErrorMessage(error.message || "An unexpected error occurred.");
@@ -27,18 +28,20 @@ const AddStockModal = ({ product, onClose, onSave }) => {
       <div className="modal-content">
         <h2>Add Stock for {product.name}</h2>
         {errorMessage && <div className="error-message">{errorMessage}</div>}
-        <div className="form-group">
+        <form onSubmit={handleSave}>
           <label>Quantity to Add</label>
           <input
             type="number"
             value={quantity}
             onChange={(e) => setQuantity(parseInt(e.target.value))}
           />
-        </div>
-        <div className="modal-actions">
-          <button onClick={handleSave}>Save</button>
-          <button onClick={onClose}>Cancel</button>
-        </div>
+          <div className="button-group">
+            <button type="submit">Save</button>
+            <button type="button" onClick={onClose}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
