ea15cfaa9e9f4f2a793dd9e83953350c701a1755
Clegane18 <126550063+Clegane18@users.noreply.github.com>
2024-08-08 16:00:26 +0800

    git add authentication in cart icon

--- src/components/onlineStoreFrontComponents/OnlineCartIcon.js
+++ src/components/onlineStoreFrontComponents/OnlineCartIcon.js
@@ -2,15 +2,27 @@ import React, { useContext } from "react";
 import { OnlineCartContext } from "./OnlineCartContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "../../styles/onlineStoreFrontComponents/OnlineCartIcon.css";
 
-const CartIcon = ({ onClick }) => {
+const CartIcon = () => {
   const { getItemCount } = useContext(OnlineCartContext);
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
   const itemCount = getItemCount();
+
+  const handleClick = () => {
+    if (!isAuthenticated) {
+      navigate("/customer-login");
+    } else {
+      navigate("/online-cart");
+    }
+  };
 
   return (
     <div id="root-online-cart-icon">
-      <div className="cart-icon-container" onClick={onClick}>
+      <div className="cart-icon-container" onClick={handleClick}>
         <FontAwesomeIcon icon={faShoppingCart} className="cart-icon" />
         {itemCount > 0 && <span className="item-count">{itemCount}</span>}
       </div>
