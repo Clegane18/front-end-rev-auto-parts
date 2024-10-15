8ce283ad88486bcfdbb656940030af9c2dacf1ad
Clegane18 <126550063+Clegane18@users.noreply.github.com>
2024-10-15 18:31:48 +0800

    refactor loading gif

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Loading.css";
 import loadingGif from "../assets/loading.gif";
 
-const Loading = () => (
-  <div id="root-loading-component">
-    <div className="loading-container">
-      <img src={loadingGif} alt="Loading..." className="loading-image" />
-      <div className="dots">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
+const Loading = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+    <div id="root-loading-component">
+      <div className="loading-container">
+        <img
+          src={loadingGif}
+          alt="Loading..."
+          className="loading-image"
+          onLoad={() => setImageLoaded(true)}
+          style={{ visibility: imageLoaded ? "visible" : "hidden" }}
+        />
+        <div
+          className="dots"
+          style={{ visibility: imageLoaded ? "visible" : "hidden" }}
+        >
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Loading;
